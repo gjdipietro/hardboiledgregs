b7abced32c037ae2a4ca84d15f4dc07ddd26442e
unknown <gjdipietro@gmail.com>
2016-04-12 09:44:11 -0400

    added serviceworker and https

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,23 +1,42 @@
+'use strict';
 
-var animation = false,
-    animationstring = 'animation',
-    keyframeprefix = '',
-    domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
-    pfx  = '',
-    elm = document.createElement('div');
+/*========================================
+REGISTER SERVICE WORKER
+=========================================*/
+(function() {
+	if ('serviceWorker' in navigator) {
+  		navigator.serviceWorker.register('/serviceWorker.js', {
+    		scope: '/'
+  		});
+	}
+})();
 
-if( elm.style.animationName !== undefined ) {
-	function myScript(){
+
+/*========================================
+REGISTER ANIMATIONS
+=========================================*/
+(function() {
+	if( elm.style.animationName !== undefined ) {
+		var animation = false,
+	    animationstring = 'animation',
+	    keyframeprefix = '',
+	    domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
+	    pfx  = '',
+	    elm = document.createElement('div');
+	    animateFooter();
+		window.addEventListener("scroll", myScript);
+		var root = document.getElementsByTagName( 'html' )[0];
+		root.className += ' hasCssAnimation';
+	}
+	function animateFooter() {
 	    var scrollY = window.pageYOffset || document.documentElement.scrollTop;
 	    var footer = document.querySelectorAll(".footer_interior");
 	    var footerPos = footer[0].getBoundingClientRect().top;
 	    if(footerPos < scrollY) {
 	      footer[0].className = "footer_interior in_view";
-	    } else {
+	    } 
+	    else {
 	      footer[0].className = "footer_interior";
 	    }
 	}
-	window.addEventListener("scroll", myScript);
-	var root = document.getElementsByTagName( 'html' )[0];
-	root.className += ' hasCssAnimation';
-}
+})();
